5032c24db7c0febe87b9c365abc4474a36dcc273
tungvv318 <tungvu3181999@gmail.com>
2019-04-04 23:58:19 +0700

    cơ bản là xong hết (còn phần xử lý input số lượng không cho nhập số âm)

--- HTML/js/detail.js
+++ HTML/js/detail.js
@@ -1,6 +1,6 @@
 function setChange(optionChange){
 	var quantity = document.getElementById('quantity');
-	if (quantity.value==1){
+	if (quantity.value == 1){
 		if(optionChange == 'reduce'){
 			quantity.value = 1;
 		}
@@ -16,4 +16,18 @@ function setChange(optionChange){
 			quantity.value++;
 		}
 	}
+}
+function openAction(evt, actionName) {
+
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+  document.getElementById(actionName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
